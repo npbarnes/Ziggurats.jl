4f5dc19a309f5aea5533e9e092b7a8689794058a
Nathan Barnes <npbarnes@alaska.edu>
2024-03-19 00:05:24 -0500

    More tests and better organization.

--- test/completed_ziggurat_tests.jl
+++ test/completed_ziggurat_tests.jl
@@ -3,11 +3,24 @@ using ZigguratTools, Test
 include("Normal.jl")
 using .Normal
 
-zig = searchziggurat(f, finv, F, 10)
+N = 10
+zig = searchziggurat(f, finv, F, N)
 
 x,y = xyvalues(zig)
 A = layerarea(zig)
-@test x[1]*y[1] + (1 - F(x[1])) ≈ A
-@test all(x[i] * (y[i+1] - y[i]) ≈ A for i in eachindex(x)[1:end-1])
+f0 = f(0)
 
-@test all(y ≈ f(x) for (x,y) in zip(x,y))
+# Number of layers
+@test length(x) == length(y) == N 
+
+# y = f(x)
+@test all(y ≈ f(x) for (x,y) in zip(x,y)) 
+
+# Base layer area
+@test x[1]*y[1] + (1 - F(x[1])) ≈ A 
+
+# Other layer areas
+@test all(x[i] * (y[i+1] - y[i]) ≈ A for i in eachindex(x)[1:end-1]) 
+
+# top layer is close to and greater than f(0)
+@test y[end] ≈ f0 && y[end] >= f0
