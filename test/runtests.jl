216afe164d3458572911cc8691eeb9810a5236e6
Nathan Barnes <npbarnes@alaska.edu>
2024-04-21 14:34:24 -0500

    Group tests

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,3 +1,6 @@
 using SafeTestsets
 
-@safetestset "Completed Ziggurat" include("completed_ziggurat_tests.jl")
+@safetestset "Ziggurat Tests" begin
+    @safetestset "Completed Ziggurat" include("completed_ziggurat_tests.jl")
+    @safetestset "Sampling Ziggurat" include("sampling_tests.jl")
+end
