f8767a99267a871f826d231a290b1715d0976fb9
Nathan Barnes <npbarnes@alaska.edu>
2024-04-21 14:35:21 -0500

    Add tests for sampling.

---
+++ test/sampling_tests.jl
@@ -0,0 +1,18 @@
+using ZigguratTools, Test
+
+include("Normal.jl")
+using .Normal
+
+N = 10
+zs = ZigguratSampler(f, finv, F, N, tailsample)
+
+x = sampleziggurat(zs)
+@test x isa Float64
+@test !isnan(x)
+@test !isinf(x)
+@test x >= 0
+
+x = symmetricsampleziggurat(zs)
+@test x isa Float64
+@test !isnan(x)
+@test !isinf(x)
