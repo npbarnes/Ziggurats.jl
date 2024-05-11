198aa7a6a684ea476ffa4b6ead4cffa7029e94b6
Nathan Barnes <npbarnes@alaska.edu>
2024-05-10 22:13:05 -0500

    Make a variety of benchmarks

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -5,11 +5,41 @@ using .Normal
 
 const SUITE = BenchmarkGroup()
 
-const Nzig = 256
-const Nsamp = 1000
+zigsizes = [128, 256]
+sampsizes = [1, 10, 100, 1000]
+
 SUITE["sampling"] = BenchmarkGroup()
+SUITE["ziggurat"] = BenchmarkGroup()
 SUITE["sampling"]["oneside"] = BenchmarkGroup()
+SUITE["sampling"]["symmetric"] = BenchmarkGroup()
+SUITE["sampling"]["oneside"]["single"] = BenchmarkGroup()
+SUITE["sampling"]["symmetric"]["single"] = BenchmarkGroup()
+
+#=
 SUITE["sampling"]["oneside"]["single"] = 
     @benchmarkable sampleziggurat(zs) setup=(zs=ZigguratSampler(f, finv, F, Nzig, tailsample))
-SUITE["sampling"]["oneside"]["multiple"] = 
-    @benchmarkable sampleziggurat(zs, Nsamp) setup=(zs=ZigguratSampler(f, finv, F, Nzig, tailsample))
+SUITE["sampling"]["symmetric"]["single"] = 
+    @benchmarkable sampleziggurat(zs) setup=(zs=ZigguratSampler(f, finv, F, Nzig, tailsample))
+=#
+
+for Nzig in zigsizes
+    zs = ZigguratSampler(f, finv, F, Nzig, tailsample)
+
+    SUITE["ziggurat"][("Nzig", Nzig)] = 
+        @benchmarkable ZigguratSampler($f, $finv, $F, $Nzig, $tailsample)
+
+    SUITE["sampling"]["oneside"]["single"][("Nzig", Nzig)] =
+        @benchmarkable sampleziggurat($zs)
+    SUITE["sampling"]["symmetric"]["single"][("Nzig", Nzig)] =
+        @benchmarkable symmetricsampleziggurat($zs)
+
+    SUITE["sampling"]["oneside"]["multiple"][("Nzig", Nzig)] = BenchmarkGroup()
+    SUITE["sampling"]["symmetric"]["multiple"][("Nzig", Nzig)] = BenchmarkGroup()
+    
+    for Nsamp in sampsizes
+        SUITE["sampling"]["oneside"]["multiple"][("Nzig", Nzig)][("Nsamp", Nsamp)] = 
+            @benchmarkable sampleziggurat($zs, $Nsamp)
+        SUITE["sampling"]["symmetric"]["multiple"][("Nzig", Nzig)][("Nsamp", Nsamp)] = 
+            @benchmarkable symmetricsampleziggurat($zs, $Nsamp)
+    end
+end
