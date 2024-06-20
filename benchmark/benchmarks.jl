dab23f00b4ed710ffb9bf509a7c3fa1d2d992f74
Nathan Barnes <npbarnes@alaska.edu>
2024-06-20 18:57:49 -0500

    Add caching params and saving result.

--- benchmark/benchmarks.jl
+++ benchmark/benchmarks.jl
@@ -1,38 +1,44 @@
 using ZigguratTools, BenchmarkTools
 
-include("../utils/Normal.jl")
-using .Normal
+using Normal
 
-const SUITE = BenchmarkGroup()
+suite = BenchmarkGroup()
 
 zigsizes = [128, 256]
 sampsizes = [1, 10, 100, 1000]
 
-SUITE["sampling"] = BenchmarkGroup()
-SUITE["ziggurat"] = BenchmarkGroup()
-SUITE["sampling"]["oneside"] = BenchmarkGroup()
-SUITE["sampling"]["symmetric"] = BenchmarkGroup()
-SUITE["sampling"]["oneside"]["single"] = BenchmarkGroup()
-SUITE["sampling"]["symmetric"]["single"] = BenchmarkGroup()
-
 for Nzig in zigsizes
     zs = ZigguratSampler(f, finv, F, Nzig, tailsample)
 
-    SUITE["ziggurat"][("Nzig", Nzig)] = 
+    suite["ziggurat"][("Nzig", Nzig)] = 
         @benchmarkable ZigguratSampler($f, $finv, $F, $Nzig, $tailsample)
 
-    SUITE["sampling"]["oneside"]["single"][("Nzig", Nzig)] =
+    suite["sampling"]["oneside"]["single"][("Nzig", Nzig)] =
         @benchmarkable sampleziggurat($zs)
-    SUITE["sampling"]["symmetric"]["single"][("Nzig", Nzig)] =
+    suite["sampling"]["symmetric"]["single"][("Nzig", Nzig)] =
         @benchmarkable symmetricsampleziggurat($zs)
-
-    SUITE["sampling"]["oneside"]["multiple"][("Nzig", Nzig)] = BenchmarkGroup()
-    SUITE["sampling"]["symmetric"]["multiple"][("Nzig", Nzig)] = BenchmarkGroup()
     
     for Nsamp in sampsizes
-        SUITE["sampling"]["oneside"]["multiple"][("Nzig", Nzig)][("Nsamp", Nsamp)] = 
+        suite["sampling"]["oneside"]["multiple"][("Nzig", Nzig)][("Nsamp", Nsamp)] = 
             @benchmarkable sampleziggurat($zs, $Nsamp)
-        SUITE["sampling"]["symmetric"]["multiple"][("Nzig", Nzig)][("Nsamp", Nsamp)] = 
+        suite["sampling"]["symmetric"]["multiple"][("Nzig", Nzig)][("Nsamp", Nsamp)] = 
             @benchmarkable symmetricsampleziggurat($zs, $Nsamp)
     end
 end
+
+# If a cache of tuned parameters already exists, use it, otherwise, tune and cache
+# the benchmark parameters. Reusing cached parameters is faster and more reliable
+# than re-tuning `suite` every time the file is included.
+paramspath = joinpath(dirname(@__FILE__), "tune.json")
+
+if isfile(paramspath)
+    loadparams!(suite, BenchmarkTools.load(paramspath)[1], :evals)
+else
+    tune!(suite)
+    BenchmarkTools.save(paramspath, params(suite))
+end
+
+results = run(suite; verbose=true)
+resultspath = joinpath(dirname(@__FILE__), "result.json")
+BenchmarkTools.save(resultspath, results)
+results
