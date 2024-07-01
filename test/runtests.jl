b21d074ab66ea9f76e147ea970f102953338173c
Nathan Barnes <npbarnes@alaska.edu>
2024-07-01 00:51:41 -0500

    Change the name of the sampling test set

--- test/runtests.jl
+++ test/runtests.jl
@@ -2,5 +2,5 @@ using SafeTestsets
 
 @safetestset "Ziggurat Tests" begin
     @safetestset "Completed Ziggurat" include("completed_ziggurat_tests.jl")
-    @safetestset "Sampling Ziggurat" include("sampling_tests.jl")
+    @safetestset "Sampling" include("sampling_tests.jl")
 end
