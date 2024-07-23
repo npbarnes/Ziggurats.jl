59790166f4e72265de5733724d065c85f3a673df
Nathan Barnes <npbarnes@alaska.edu>
2024-07-23 14:43:08 -0500

    Add a comment explaining the bug being tested for.

--- test/regression_tests.jl
+++ test/regression_tests.jl
@@ -4,7 +4,13 @@ include("./testutils.jl")
 
 dist = SteppedExponential()
 
-# This distribution, with this N causes an error if we use a naive search algorithm
+# This distribution, with this N causes an error if we use a naive search
+# algorithm. For an continuous pdf, `zig.y = pdf.(zig.x)`, but when the pdf is
+# discontinuous, that need not be the case. We only require `zig.x = ipdf.(zig.y)`, for a
+# generalized inverse. Therefore, there are multiple valid y's for each x at the
+# discontinuity. The ziggurat search algorithm needs to be aware of this. With
+# SteppedExponential and 256 layers, the correct first layer needs to have
+# pdf(prevfloat(8.0)) < y < pdf(8.0)
 @test_nowarn UnboundedDecreasingZiggurat(
     x->pdf(dist,x),
     y->ipdf_right(dist,y),
