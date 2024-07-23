6ce6be704dfc42663b8a1c36abb9880289f40d31
Nathan Barnes <npbarnes@alaska.edu>
2024-07-22 19:31:53 -0500

    Update test suite using new interface.

--- test/completed_ziggurat_tests.jl
+++ test/completed_ziggurat_tests.jl
@@ -1,25 +1,81 @@
+
 using ZigguratTools, Test
+using Distributions
 
-include("./Normal.jl")
+function test_layer_properties(dist, N, z::UnboundedDecreasingZiggurat)
+    x = z.x
+    y = z.y
+    A = z.layerarea
 
-N = 10
-zs = ZigguratSampler(f, finv, F, N, tailsample)
+    # Number of layers
+    @test length(x) == length(y) == N
 
-x,y = xyvalues(zs)
-A = layerarea(zs)
-f0 = f(0)
+    # Each layer has positive thickness
+    @test y[begin] > zero(y[begin])
+    @test all(y[i+1] > y[i] for i in eachindex(y)[begin:end-1])
 
-# Number of layers
-@test length(x) == length(y) == N 
+    # Ziggurats never get wider (they can get narrower or stay the same width)
+    @test all(x[i+1] <= x[i] for i in eachindex(x)[begin:end-1])
 
-# y = f(x)
-@test all(y ≈ f(x) for (x,y) in zip(x,y)) 
+    # y = f(x)
+    @test all(y .≈ pdf.(dist, x))
 
-# Base layer area
-@test x[1]*y[1] + (1 - F(x[1])) ≈ A 
+    # Base layer area
+    @test x[1]*y[1] + ccdf(dist, x[1]) ≈ A 
 
-# Other layer areas
-@test all(x[i] * (y[i+1] - y[i]) ≈ A for i in eachindex(x)[1:end-1]) 
+    # Other layer areas
+    @test all(x[i] * (y[i+1] - y[i]) ≈ A for i in eachindex(x)[1:end-1]) 
 
-# top layer is close to and greater than f(0)
-@test y[end] ≈ f0 && y[end] >= f0
+    # Upper boundary is close to and greater than or equal to f(m)
+    @test y[end] ≈ pdf(dist, mode(dist)) && y[end] >= pdf(dist, mode(dist))
+end
+
+@testset "Normal (x>=0)" begin
+    dist = Normal()
+    N = 256
+    
+    z = UnboundedDecreasingZiggurat(
+        x->pdf(dist,x),
+        y->ipdf_right(dist,y),
+        x->ccdf(dist,x),
+        mode(dist),
+        N,
+        x->sampler(truncated(dist, lower=x))
+    )
+
+    test_layer_properties(dist, N, z)
+end
+
+@testset "Exponential" begin
+    dist = Exponential()
+    N = 256
+
+    z = UnboundedDecreasingZiggurat(
+        x->pdf(dist,x),
+        y->ipdf_right(dist,y),
+        x->ccdf(dist,x),
+        mode(dist),
+        N,
+        x->sampler(truncated(dist, lower=x))
+    )
+
+    test_layer_properties(dist, N, z)
+end
+
+@testset "SteppedExponential" begin
+    include("./testutils.jl")
+
+    dist = SteppedExponential()
+    N = 256
+
+    z = UnboundedDecreasingZiggurat(
+        x->pdf(dist,x),
+        y->ipdf_right(dist,y),
+        x->ccdf(dist,x),
+        mode(dist),
+        N,
+        x->sampler(truncated(dist, lower=x))
+    )
+
+    test_layer_properties(dist, N, z)
+end
--- test/sampling_tests.jl
+++ test/sampling_tests.jl
@@ -1,49 +1,182 @@
 using ZigguratTools, Test
+using Distributions
+using Random
+using Printf
+using StatsBase
 
-include("./Normal.jl")
+function test_samples(s::AbstractZiggurat,    # the sampleable instance
+    distr::ContinuousUnivariateDistribution,  # corresponding distribution
+    n::Int;                                   # number of samples to generate
+    nbins::Int=50,                            # divide the main interval into nbins
+    q::Float64=1.0e-6,                        # confidence interval, 1 - q as confidence
+    verbose::Bool=false,                      # show intermediate info (for debugging)
+    rng::Union{AbstractRNG,Missing}=missing) # add an rng?
 
-Nzig = 10
-Nsamp = 5
+    # The basic idea
+    # ------------------
+    #   Generate n samples, divide the interval [q00, q99] into nbins bins, where
+    #   q01 and q99 are 0.01 and 0.99 quantiles, and count the numbers of samples
+    #   falling into each bin. For each bin, we will compute a confidence interval
+    #   of the number, and see whether the actual number is in this interval.
+    #
+    #   If the distribution has a bounded range, it also checks whether
+    #   the samples are all within this range.
+    #
+    #   By setting a small q, we ensure that failure of the tests rarely
+    #   happen in practice.
+    #
 
-zs = ZigguratSampler(f, finv, F, Nzig, tailsample)
+    verbose && println("test_samples on $(typeof(s))")
 
-x = sampleziggurat(zs)
-@test x isa Float64
-@test !isnan(x)
-@test !isinf(x)
-@test x >= 0
+    n > 1 || error("The number of samples must be greater than 1.")
+    nbins > 1 || error("The number of bins must be greater than 1.")
+    0.0 < q < 0.1 || error("The value of q must be within the open interval (0.0, 0.1).")
 
-xs = sampleziggurat(zs, Nsamp)
-@test length(xs) == Nsamp
-@test xs isa Vector{Float64}
-@test all(!isnan, xs)
-@test all(!isinf, xs)
-@test all(>=(0), xs)
+    # determine the range of values to examine
+    vmin = minimum(distr)
+    vmax = maximum(distr)
 
-out = Vector{Float64}(undef, Nsamp)
-xs = sampleziggurat!(out, zs)
-@test xs === out
-@test length(xs) == Nsamp
-@test xs isa Vector{Float64}
-@test all(!isnan, xs)
-@test all(!isinf, xs)
-@test all(>=(0), xs)
+    local rmin::Float64
+    local rmax::Float64
+    if applicable(quantile, distr, 0.5)
+        rmin = quantile(distr, 0.01)
+        rmax = quantile(distr, 0.99)
+    elseif isfinite(vmin) && isfinite(vmax)
+        rmin = vmin
+        rmax = vmax
+    end
+    edges = range(rmin, rmax, nbins + 1)
 
-x = symmetricsampleziggurat(zs)
-@test x isa Float64
-@test !isnan(x)
-@test !isinf(x)
+    # determine confidence intervals for counts:
+    # with probability q, the count will be out of this interval.
+    #
+    clb = Vector{Int}(undef, nbins)
+    cub = Vector{Int}(undef, nbins)
+    cdfs = map(Base.Fix1(cdf, distr), edges)
 
-xs = symmetricsampleziggurat(zs, Nsamp)
-@test length(xs) == Nsamp
-@test xs isa Vector{Float64}
-@test all(!isnan, xs)
-@test all(!isinf, xs)
+    for i = 1:nbins
+        pi = cdfs[i+1] - cdfs[i]
+        bp = Binomial(n, pi)
+        clb[i] = floor(Int, quantile(bp, q / 2))
+        cub[i] = ceil(Int, cquantile(bp, q / 2))
+        @assert cub[i] >= clb[i]
+    end
 
-out = Vector{Float64}(undef, Nsamp)
-xs = symmetricsampleziggurat!(out, zs)
-@test xs === out
-@test length(xs) == Nsamp
-@test xs isa Vector{Float64}
-@test all(!isnan, xs)
-@test all(!isinf, xs)
+    # generate samples using RNG passed or default RNG
+    # we also check reproducibility
+    if rng === missing
+        Random.seed!(1234)
+        samples = rand(s, n)
+        Random.seed!(1234)
+        samples2 = rand(s, n)
+    else
+        rng2 = deepcopy(rng)
+        samples = rand(rng, s, n)
+        samples2 = rand(rng2, s, n)
+    end
+    @test length(samples) == n
+    @test samples2 == samples
+
+    if isa(distr, StudentizedRange)
+        samples[isnan.(samples)] .= 0.0 # Underlying implementation in Rmath can't handle very low values.
+    end
+
+    # check whether all samples are in the valid range
+    for i = 1:n
+        @inbounds si = samples[i]
+        vmin <= si <= vmax ||
+            error("Sample value out of valid range.")
+    end
+
+    # get counts
+    cnts = fit(Histogram, samples, edges; closed=:right).weights
+    @assert length(cnts) == nbins
+
+    # check the counts
+    for i = 1:nbins
+        if verbose
+            @printf("[%.4f, %.4f) ==> (%d, %d): %d\n", edges[i], edges[i+1], clb[i], cub[i], cnts[i])
+        end
+        clb[i] <= cnts[i] <= cub[i] ||
+            error("The counts are out of the confidence interval.")
+    end
+    return samples
+end
+
+function testsampling(dist, z)
+    values = test_samples(z, dist, 10000)
+
+    @test eltype(values) === eltype(dist)
+    @test !any(isnan, values)
+    @test !any(isinf, values)
+end
+
+@testset "Normal (x>=0)" begin
+    basedist = Normal()
+
+    # Because of the choice of UnboundedDecreasingZiggurat, ipdf_right, and ccdf,
+    # this ziggurat will actually be sampling from truncated(Normal(), lower=0.0).
+    # A small number N is chosen so that the fallback branch gets chosen with high
+    # probability and its confidence intervals can be tested.
+    z = UnboundedDecreasingZiggurat(
+        x->pdf(basedist,x),
+        y->ipdf_right(basedist,y),
+        x->ccdf(basedist,x),
+        mode(basedist),
+        2,
+        x->sampler(truncated(basedist, lower=x))
+    )
+
+    testsampling(truncated(basedist, lower=mode(basedist)), z)
+end
+
+@testset "Normal (x<=0)" begin
+    basedist = Normal()
+
+    # Because of the choice of a AbstractUnboundedMonotonicZiggurat, ipdf_left, and cdf,
+    # this ziggurat will actually be sampling from truncated(Normal(), lower=0.0).
+    # A small number N is chosen so that the fallback branch gets chosen with high
+    # probability and its confidence intervals can be tested.
+    z = UnboundedIncreasingZiggurat(
+        x->pdf(basedist,x),
+        y->ipdf_left(basedist,y),
+        x->cdf(basedist,x),
+        mode(basedist),
+        2,
+        x->sampler(truncated(basedist, upper=x))
+    )
+
+    testsampling(truncated(basedist, upper=mode(basedist)), z)
+end
+
+@testset "Exponential" begin
+    basedist = Exponential()
+
+    z = UnboundedDecreasingZiggurat(
+        x->pdf(basedist,x),
+        y->ipdf_right(basedist,y),
+        x->ccdf(basedist,x),
+        mode(basedist),
+        2,
+        x->sampler(truncated(basedist, lower=x))
+    )
+
+    testsampling(basedist, z)
+end
+
+@testset "SteppedExponential" begin
+    include("./testutils.jl")
+    
+    basedist = SteppedExponential()
+
+    z = UnboundedDecreasingZiggurat(
+        x->pdf(basedist,x),
+        y->ipdf_right(basedist,y),
+        x->ccdf(basedist,x),
+        mode(basedist),
+        2,
+        x->sampler(truncated(basedist, lower=x))
+    )
+
+    testsampling(basedist, z)
+end
