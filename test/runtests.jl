d2ec16f87cb5b2a8e159c594f8f29ef7bf2d535e
Nathan Barnes <npbarnes@alaska.edu>
2024-08-04 21:14:47 -0500

    Test more cases of test-distributions

--- test/runtests.jl
+++ test/runtests.jl
@@ -15,10 +15,13 @@ include("Doorstop.jl")
 @testset "Stepped Exponential Distributions" begin
     test_distr(SteppedExponential(), 10^6)
     test_distr(SteppedExponential(0.5), 10^6)
+    test_distr(SteppedExponential(1.5), 10^6)
 end
 
 @testset "Doorstop Distribution" begin
     test_distr(Doorstop(-1, 1, 3), 10^6)
+    test_distr(Doorstop(-1, -1, 3), 10^6)
+    test_distr(Doorstop(-1, 1, 1), 10^6)
 end
 
 # Test ZigguratTools
