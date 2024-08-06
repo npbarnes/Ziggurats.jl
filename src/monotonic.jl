c0184b9b7f07a65d02104271873ccc04ee748240
Nathan Barnes <npbarnes@alaska.edu>
2024-08-05 23:01:48 -0500

    Function order and formatting

--- src/monotonic.jl
+++ src/monotonic.jl
@@ -38,89 +38,6 @@ function monotonic_ziggurat(N, modalboundary, tailarea::Function, pdf, ipdf, tai
     UnboundedMonotonicZiggurat(x, y, pdf, modalboundary, tailsampler(x[2]))
 end
 
-## Construction
-
-# Bounded support
-layerarea(y2, modalboundary, argminboundary) = abs(argminboundary - modalboundary) * y2
-
-# Unbounded support
-function layerarea(y2, x2, modalboundary, tailarea::Function)
-    if y2 == zero(y2)
-        return zero(x2 * y2)
-    end
-    abs(x2 - modalboundary) * y2 + tailarea(x2)
-end
-
-function initialize!(y, y2)
-    y[1] = zero(eltype(y))
-    y[2] = y2
-end
-
-function finalize!(x, y, modalboundary, A, ipdf, modalpdf)
-    for i in eachindex(x)[(begin + 1):(end - 1)]
-        if y[i] >= modalpdf
-            # failed to build ziggurat, y2 is too large.
-            return nothing
-        end
-        x[i] = ipdf(y[i])
-        y[i + 1] = A / abs(x[i] - modalboundary) + y[i]
-    end
-
-    if y[end] <= modalpdf
-        x[end] = ipdf(y[end])
-    else
-        x[end] = modalboundary
-    end
-
-    x, y
-end
-
-# Caller is responsible for ensuring consistancy of the build arguments. I.e.,
-# 1) length(x) == length(y) >= 1
-# 2) zero(y2) < y2 <= pdf(boundary)
-# 3) boundarypdf = pdf(boundary)
-# 4) the pdf is monotonic
-# 5) the ipdf is the gerneralized inverse of pdf 
-#       (i.e. ipdf(y) is the largest x such that pdf(x) >= y for decreasing pdfs,
-#        and ipdf(y) is the smallest x such that pdf(x) >= y for increasing pdfs)
-
-# Bounded support
-function build!(x, y, y2, modalboundary, argminboundary::Number, ipdf, modalpdf)
-    initialize!(y, y2)
-
-    A = layerarea(y[2], modalboundary, argminboundary)
-    x[1] = argminboundary
-
-    finalize!(x, y, modalboundary, A, ipdf, modalpdf)
-end
-
-# Unbounded support
-function build!(x, y, y2, modalboundary, tailarea::Function, ipdf, modalpdf)
-    initialize!(y, y2)
-
-    x2 = ipdf(y2)
-    if isinf(x2)
-        # TODO: find a more consistant way for build!() to indicate when y2 is
-        # too small or too large.
-
-        # y2 is too small
-        y[end] = zero(eltype(y))
-        return x, y
-    end
-    A = layerarea(y[2], x2, modalboundary, tailarea)
-
-    if x2 == modalboundary
-        s = sign(ipdf(y2 / 2) - modalboundary)
-    else
-        s = sign(x2 - modalboundary)
-    end
-
-    x[1] = modalboundary + s * A / y[2]
-
-    finalize!(x, y, modalboundary, A, ipdf, modalpdf)
-end
-
-## Searching
 """
     search(N, modalboundary, argminboundary::Number, pdf, ipdf)
     search(N, modalboundary, tailarea::Function, pdf, ipdf)
@@ -163,7 +80,106 @@ function search(N, modalboundary, tail, pdf, ipdf)
     build!(x, y, ystar, modalboundary, tail, ipdf, modalpdf)
 end
 
+# The caller of build!() is responsible for ensuring consistancy of the build
+# arguments. I.e.,
+# 1) length(x) == length(y) >= 1
+# 2) zero(y2) < y2 <= pdf(boundary)
+# 3) boundarypdf = pdf(boundary)
+# 4) the pdf is monotonic
+# 5) the ipdf is the gerneralized inverse of pdf (i.e. ipdf(y) is the largest x
+#       such that pdf(x) >= y for decreasing pdfs, and ipdf(y) is the smallest x
+#       such that pdf(x) >= y for increasing pdfs)
+
+# Bounded support
+function build!(x, y, y2, modalboundary, argminboundary::Number, ipdf, modalpdf)
+    initialize!(y, y2)
+
+    A = layerarea(y[2], modalboundary, argminboundary)
+    x[1] = argminboundary
+
+    finalize!(x, y, modalboundary, A, ipdf, modalpdf)
+end
+
+# Unbounded support
+function build!(x, y, y2, modalboundary, tailarea::Function, ipdf, modalpdf)
+    initialize!(y, y2)
+
+    x2 = ipdf(y2)
+    if isinf(x2)
+        # TODO: find a more consistant way for build!() to indicate when y2 is
+        # too small or too large.
+
+        # y2 is too small
+        y[end] = zero(eltype(y))
+        return x, y
+    end
+    A = layerarea(y[2], x2, modalboundary, tailarea)
+
+    if x2 == modalboundary
+        s = sign(ipdf(y2 / 2) - modalboundary)
+    else
+        s = sign(x2 - modalboundary)
+    end
+
+    x[1] = modalboundary + s * A / y[2]
+
+    finalize!(x, y, modalboundary, A, ipdf, modalpdf)
+end
+
+function initialize!(y, y2)
+    y[1] = zero(eltype(y))
+    y[2] = y2
+end
+
+# Bounded support
+layerarea(y2, modalboundary, argminboundary) = abs(argminboundary - modalboundary) * y2
+
+# Unbounded support
+function layerarea(y2, x2, modalboundary, tailarea::Function)
+    if y2 == zero(y2)
+        return zero(x2 * y2)
+    end
+    abs(x2 - modalboundary) * y2 + tailarea(x2)
+end
+
+function finalize!(x, y, modalboundary, A, ipdf, modalpdf)
+    for i in eachindex(x)[(begin + 1):(end - 1)]
+        if y[i] >= modalpdf
+            # failed to build ziggurat, y2 is too large.
+            return nothing
+        end
+        x[i] = ipdf(y[i])
+        y[i + 1] = A / abs(x[i] - modalboundary) + y[i]
+    end
+
+    if y[end] <= modalpdf
+        x[end] = ipdf(y[end])
+    else
+        x[end] = modalboundary
+    end
+
+    x, y
+end
+
 ## Sampling
+function Base.rand(rng::AbstractRNG, z::MonotonicZiggurat)
+    N = length(z.x) - 1 # number of layers
+
+    while true
+        l = rand(rng, 1:N)
+        x = (z.modalboundary - z.x[l]) * rand(rng) + z.x[l]
+
+        if between(z.x[l + 1], z.modalboundary, x)
+            return x
+        else
+            sp = slowpath(rng, z, l, x)
+            if sp !== nothing
+                return sp
+            end
+        end
+    end
+end
+
 function between(a, b, x)
     l, r = minmax(a, b)
     l <= x <= r
@@ -187,21 +203,3 @@ function slowpath(rng, z::UnboundedMonotonicZiggurat, l, x)
 end
 
 slowpath(rng, z::BoundedMonotonicZiggurat, l, x) = simple_rejection(rng, z, l, x)
-
-function Base.rand(rng::AbstractRNG, z::MonotonicZiggurat)
-    N = length(z.x) - 1 # number of layers
-
-    while true
-        l = rand(rng, 1:N)
-        x = (z.modalboundary - z.x[l]) * rand(rng) + z.x[l]
-
-        if between(z.x[l + 1], z.modalboundary, x)
-            return x
-        else
-            sp = slowpath(rng, z, l, x)
-            if sp !== nothing
-                return sp
-            end
-        end
-    end
-end
