c05ff70c807dbdddd4d10a4f00755992bab01fd3
Nathan Barnes <npbarnes@alaska.edu>
2024-08-18 14:40:38 -0500

    Testsets don't need to have test in the name.

--- test/ziggurat_tests.jl
+++ test/ziggurat_tests.jl
@@ -1,4 +1,4 @@
-@testset "Ziggurat Tests" begin
+@testset "Ziggurats" begin
     @testset "Completed Ziggurat" include("completed_ziggurat_tests.jl")
     @testset "Sampling" include("sampling_tests.jl")
 end
