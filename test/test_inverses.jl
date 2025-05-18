664b9679704f8ebdd01ef89a65d5e311eb088a91
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-05-17 23:52:28 -0500

    No longer require a specific result when many zeros are present.

--- test/test_inverses.jl
+++ test/test_inverses.jl
@@ -34,50 +34,50 @@
         @test_throws "No inverse exists" inverse(cos, (0, π))(2)
         @test inverse(cos, (0, π))(-2) == float(π)
 
-        @test inverse(x -> s_curve(-x), (-2, 2))(-1) == 2
-        @test inverse(x -> s_curve(-x), (-2, 2))(0) == 0
-        @test inverse(x -> s_curve(-x), (-2, 2))(1) == -1
+        @test inverse(x -> s_curve(-x), (-2, 2))(-1) >= 1
+        @test inverse(x -> s_curve(-x), (-2, 2))(0) ≈ 0 atol=1e-16
+        @test -2 <= inverse(x -> s_curve(-x), (-2, 2))(1) <= -1
 
-        @test inverse(x -> heaviside1(-x), (-1, 1))(0) == 1
-        @test inverse(x -> heaviside1(-x), (-1, 1))(0.5) == 0.0
-        @test inverse(x -> heaviside1(-x), (-1, 1))(1) == 0.0
+        @test inverse(x -> heaviside1(-x), (-1, 1))(0) > 0
+        @test inverse(x -> heaviside1(-x), (-1, 1))(0.5) ≈ 0.0 atol=1e-16
+        @test -1 <= inverse(x -> heaviside1(-x), (-1, 1))(1) <= 0.0
 
-        @test inverse(x -> heaviside2(-x), (-1, 1))(0) == 1
-        @test inverse(x -> heaviside2(-x), (-1, 1))(0.5) == prevfloat(0.0)
-        @test inverse(x -> heaviside2(-x), (-1, 1))(1) == prevfloat(0.0)
+        @test inverse(x -> heaviside2(-x), (-1, 1))(0) >= 0
+        @test inverse(x -> heaviside2(-x), (-1, 1))(0.5) ≈ 0.0 atol=1e-16
+        @test -1 <= inverse(x -> heaviside2(-x), (-1, 1))(1) < 0
 
-        @test inverse(x -> heaviside3(-x), (-1, 1))(0) == 1
-        @test inverse(x -> heaviside3(-x), (-1, 1))(0.25) == 0.0
-        @test inverse(x -> heaviside3(-x), (-1, 1))(0.5) == 0.0
-        @test inverse(x -> heaviside3(-x), (-1, 1))(0.75) == prevfloat(0.0)
-        @test inverse(x -> heaviside3(-x), (-1, 1))(1) == prevfloat(0.0)
+        @test inverse(x -> heaviside3(-x), (-1, 1))(0) > 0
+        @test inverse(x -> heaviside3(-x), (-1, 1))(0.25) ≈ 0 atol=1e-16
+        @test inverse(x -> heaviside3(-x), (-1, 1))(0.5) ≈ 0 atol=1e-16
+        @test inverse(x -> heaviside3(-x), (-1, 1))(0.75) ≈ 0 atol=1e-16
+        @test inverse(x -> heaviside3(-x), (-1, 1))(1) < 0
 
         # Increasing functions
         @test inverse(cos, (-π, 0))(0) == -π / 2
 
         @test_throws "No inverse exists" inverse(cos, (-π, 0))(2)
-        @test inverse(cos, (-π, 0))(-2) == float(-π)
+        @test inverse(cos, (-π, 0))(-2) ≈ float(-π)
 
-        @test inverse(s_curve, (-2, 2))(-1) == -2
-        @test inverse(s_curve, (-2, 2))(0) == 0
-        @test inverse(s_curve, (-2, 2))(1) == 1
+        @test inverse(s_curve, (-2, 2))(-1) <= -1
+        @test inverse(s_curve, (-2, 2))(0) ≈ 0 atol=1e-16
+        @test inverse(s_curve, (-2, 2))(1) >= 1
 
-        @test inverse(heaviside1, (-1, 1))(0) == -1
-        @test inverse(heaviside1, (-1, 1))(0.5) == 0.0
-        @test inverse(heaviside1, (-1, 1))(1) == 0.0
+        @test inverse(heaviside1, (-1, 1))(0) < 0
+        @test inverse(heaviside1, (-1, 1))(0.5) ≈ 0 atol=1e-16
+        @test inverse(heaviside1, (-1, 1))(1) >= 0
 
-        @test inverse(heaviside2, (-1, 1))(0) == -1
-        @test inverse(heaviside2, (-1, 1))(0.5) == nextfloat(0.0)
-        @test inverse(heaviside2, (-1, 1))(1) == nextfloat(0.0)
+        @test inverse(heaviside2, (-1, 1))(0) <= 0
+        @test inverse(heaviside2, (-1, 1))(0.5) ≈ 0 atol=1e-16
+        @test inverse(heaviside2, (-1, 1))(1) > 0
 
-        @test inverse(heaviside3, (-1, 1))(0) == -1
-        @test inverse(heaviside3, (-1, 1))(0.25) == 0.0
-        @test inverse(heaviside3, (-1, 1))(0.5) == 0.0
-        @test inverse(heaviside3, (-1, 1))(0.75) == nextfloat(0.0)
-        @test inverse(heaviside3, (-1, 1))(1) == nextfloat(0.0)
+        @test inverse(heaviside3, (-1, 1))(0) < 0
+        @test inverse(heaviside3, (-1, 1))(0.25) ≈ 0 atol=1e-16
+        @test inverse(heaviside3, (-1, 1))(0.5) ≈ 0 atol=1e-16
+        @test inverse(heaviside3, (-1, 1))(0.75) ≈ 0 atol=1e-16
+        @test inverse(heaviside3, (-1, 1))(1) > 0
 
-        # Non-monotonicity is sometimes detected
-        @test_throws "f must be monotonic" inverse(cos, (-1.3, π))(0)
+        # Non-monotonicity is not detected
+        @test -1.3 <= inverse(cos, (-1.3, π))(0) <= π
 
         # Non-monotonicity is not always detected. Ideally, it would be, but that's
         # probably impossible to do in general.
@@ -93,15 +93,8 @@
             false # test fails if no error is thrown
         end
 
-        # Constant functions are not allowed.
-        @test_throws "f must be non-constant" inverse(x -> 1.0, (-1, 1))(1)
-        @test_throws "f must be non-constant" inverse(sign, (-2, -1))(-1)
-        @test_throws "f must be non-constant" inverse(sign, (1, 2))(1)
-
-        # These have no solution, but f being constant is detected first.
-        @test_throws "f must be non-constant" inverse(x -> 1.0, (-1, 1))(2)
-        @test_throws "f must be non-constant" inverse(sign, (-2, -1))(-2)
-        @test_throws "f must be non-constant" inverse(sign, (1, 2))(2)
+        # Constant functions are allowed.
+        @test -1 <= inverse(x -> 1.0, (-1, 1))(1) <= 1
 
         # Sometimes constant functions and non-monotonic functions get mixed up.
         @test_broken try
@@ -119,15 +112,15 @@
         end
 
         # Domains that include some positive numbers
-        @test_throws "no solutions" inverse(sign, (-Inf, Inf))(2.0)
-        @test_throws "no solutions" inverse(sign, (-Inf, 1.0))(2.0)
-        @test_throws "no solutions" inverse(sign, (-1.0, Inf))(2.0)
-        @test_throws "no solutions" inverse(sign, (-1.0, 1.0))(2.0)
+        @test_throws "ArgumentError: No inverse" inverse(sign, (-Inf, Inf))(2.0)
+        @test_throws "ArgumentError: No inverse" inverse(sign, (-Inf, 1.0))(2.0)
+        @test_throws "ArgumentError: No inverse" inverse(sign, (-1.0, Inf))(2.0)
+        @test_throws "ArgumentError: No inverse" inverse(sign, (-1.0, 1.0))(2.0)
 
-        @test inverse(sign, (-Inf, Inf))(1.0) == nextfloat(0.0)
-        @test inverse(sign, (-Inf, 1.0))(1.0) == nextfloat(0.0)
-        @test inverse(sign, (-1.0, Inf))(1.0) == nextfloat(0.0)
-        @test inverse(sign, (-1.0, 1.0))(1.0) == nextfloat(0.0)
+        @test inverse(sign, (-Inf, Inf))(1.0) > 0
+        @test inverse(sign, (-Inf, 1.0))(1.0) > 0
+        @test inverse(sign, (-1.0, Inf))(1.0) > 0
+        @test inverse(sign, (-1.0, 1.0))(1.0) > 0
 
         @test inverse(sign, (-Inf, Inf))(0.0) == 0.0
         @test inverse(sign, (-Inf, 1.0))(0.0) == 0.0
@@ -145,9 +138,9 @@
         @test inverse(sign, (-1.0, 1.0))(-2.0) == -1.0
 
         @test_throws "No inverse exists" inverse(sign, (-10.0, 10.0))(2.0)
-        @test inverse(sign, (-10.0, 10.0))(1.0) == nextfloat(0.0)
-        @test inverse(sign, (-10.0, 10.0))(0.0) == 0.0
-        @test inverse(sign, (-10.0, 10.0))(-1.0) == -10
+        @test inverse(sign, (-10.0, 10.0))(1.0) > 0
+        @test inverse(sign, (-10.0, 10.0))(0.0) ≈ 0.0 atol = 1e-16
+        @test inverse(sign, (-10.0, 10.0))(-1.0) < 0
         @test inverse(sign, (-10.0, 10.0))(-2.0) == -10.0
 
         # domain ends at zero
@@ -174,11 +167,11 @@
 
         msign = x -> -sign(x)
         @test_throws "No inverse exists" inverse(msign, (-10.0, 10.0))(2.0)
-        @test inverse(msign, (-10.0, 10.0))(1.0) == prevfloat(0.0)
-        @test inverse(msign, (-10.0, 10.0))(0.5) == prevfloat(0.0)
-        @test inverse(msign, (-10.0, 10.0))(0.0) == 0.0
-        @test inverse(msign, (-10.0, 10.0))(-0.5) == 0.0
-        @test inverse(msign, (-10.0, 10.0))(-1.0) == 10.0
+        @test inverse(msign, (-10.0, 10.0))(1.0) < 0.0
+        @test inverse(msign, (-10.0, 10.0))(0.5) ≈ 0.0 atol = 1e-16
+        @test inverse(msign, (-10.0, 10.0))(0.0) ≈ 0.0 atol = 1e-16
+        @test inverse(msign, (-10.0, 10.0))(-0.5) ≈ 0.0 atol = 1e-16
+        @test inverse(msign, (-10.0, 10.0))(-1.0) > 0.0
         @test inverse(msign, (-10.0, 10.0))(-2.0) == 10.0
     end
 end
