67ebdd907a95b559e09a34372f63f74fe0342f94
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-06-08 18:50:34 -0500

    Add tests for _get_tailarea_func().

--- test/interface_tests.jl
+++ test/interface_tests.jl
@@ -27,4 +27,41 @@
             @test z_call.modalboundary == z_lamb.modalboundary == z_func.modalboundary
         end
     end
+
+    @testset "Argument checking utilities" begin
+        @testset "_choose_tailarea_func" begin
+            inc_func(x) = x
+            dec_func(x) = -x
+
+            notnothing1 = x->1
+            notnothing2 = x->2
+            notnothing3 = x->3
+            
+            #! format: off
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], nothing, nothing, nothing) === nothing
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], nothing, nothing, nothing) === nothing
+
+            @test_throws "a ccdf is provided"   ZigguratTools._choose_tailarea_func(inc_func, [0,1], nothing, nothing, notnothing1)
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], nothing, nothing, notnothing1) === notnothing1
+
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], nothing, notnothing1, nothing) === notnothing1
+            @test_throws "a cdf is provided"    ZigguratTools._choose_tailarea_func(dec_func, [0,1], nothing, notnothing1, nothing)
+
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], nothing, notnothing1, notnothing2) === notnothing1
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], nothing, notnothing1, notnothing2) === notnothing2
+
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], notnothing1, nothing, nothing) === notnothing1
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], notnothing1, nothing, nothing) === notnothing1
+
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], notnothing1, nothing, notnothing2) === notnothing1
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], notnothing1, nothing, notnothing2) === notnothing1
+            
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], notnothing1, notnothing2, nothing) === notnothing1
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], notnothing1, notnothing2, nothing) === notnothing1
+
+            @test                               ZigguratTools._choose_tailarea_func(inc_func, [0,1], notnothing1, notnothing2, notnothing3) === notnothing1
+            @test                               ZigguratTools._choose_tailarea_func(dec_func, [0,1], notnothing1, notnothing2, notnothing3) === notnothing1
+            #! format: on
+        end
+    end
 end
