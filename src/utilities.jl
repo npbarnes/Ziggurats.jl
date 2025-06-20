a3c4de2875e75dcccc85431bb4417a138c21d43b
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-06-20 00:35:53 -0500

    Add more interface functions to Regularized.

--- src/utilities.jl
+++ src/utilities.jl
@@ -14,3 +14,6 @@ end
 
 Base.getindex(r::Regularized, i) = r.a[i]
 Base.extrema(r::Regularized) = extrema(r.a)
+Base.length(r::Regularized) = length(r.a)
+Base.iterate(r::Regularized) = iterate(r.a)
+Base.iterate(r::Regularized, s) = iterate(r.a, s)
