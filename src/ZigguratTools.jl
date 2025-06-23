ef905750f3ce3d640536ca62093cea37ec070736
Nathan Barnes <nathanpaulbarnes@gmail.com>
2025-06-22 19:42:46 -0500

    Add top level ziggurat() function automating ziggurat type selection.

--- src/ZigguratTools.jl
+++ src/ZigguratTools.jl
@@ -11,7 +11,7 @@ using FixedPointNumbers
 
 export Ziggurat, MonotonicZiggurat
 export BoundedZiggurat, UnboundedZiggurat, CompositeZiggurat
-export monotonic_ziggurat
+export ziggurat, monotonic_ziggurat
 export inversepdf
 
 public monotonic_segments, NoWrap
@@ -22,5 +22,43 @@ include("utilities.jl")
 include("inverse_pdf.jl")
 include("monotonic.jl")
 include("composite.jl")
+
+function ziggurat(
+    pdf,
+    domain,
+    N = nothing;
+    ipdf = nothing,
+    tailarea = nothing,
+    cdf = nothing,
+    ccdf = nothing,
+    left_fallback = nothing,
+    right_fallback = nothing,
+    fallback = nothing,
+    p = nothing
+)
+    domain = regularize(domain)
+
+    if N === nothing
+        N = eltype(domain) == Float16 ? 64 : 256
+    end
+
+    if length(domain) < 2
+        error("The domain needs at least two distinct points to mark the boundaries.")
+    elseif length(domain) == 2
+        monotonic_ziggurat(pdf, domain, N; ipdf, tailarea, cdf, ccdf, fallback)
+    else
+        CompositeZiggurat(
+            pdf,
+            domain,
+            N;
+            ipdfs = ipdf,
+            cdf,
+            ccdf,
+            left_fallback,
+            right_fallback,
+            p
+        )
+    end
+end
 
 end # module
