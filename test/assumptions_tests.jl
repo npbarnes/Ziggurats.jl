0d610b174f28a51ceae3323714e16c033808526f
Nathan Barnes <nathanpaulbarnes@gmail.com>
2026-01-06 19:46:02 +0000

    Rewrite assumptions

--- test/assumptions_tests.jl
+++ test/assumptions_tests.jl
@@ -1,40 +1,39 @@
-@testset "pdf is never evaluated at infinity" begin
-    f = x -> isinf(x) ? error("pdf evaluated at infinity") : exp(-x)
+@testset "Misc assumptions" begin
+    @testset "pdf is never evaluated at infinity" begin
+        f = x -> isinf(x) ? error("pdf evaluated at infinity") : exp(-x)
 
-    @testset "PDFWrap doesn't evaluate pdf at infinity" begin
-        try
+        @testset "PDFWrap doesn't evaluate pdf at infinity" begin
             wf = Ziggurats.PDFWrap(f, 0, Inf)
-        catch
-            result = false
-        else
-            result = true
+            @test wf(Inf) == 0
         end
 
-        @test result
-
-        try
-            wf(Inf)
-        catch
-            result = false
-        else
-            result = true
+        @testset "end to end, pdf is not evaluated at infinity" begin
+            @testset for T in TestTypes, N in [1, 2, 3, 4]
+                z = ziggurat(f, (T(0), T(Inf)), N)
+                @test rand(z, 1000) isa Any
+            end
         end
-
-        @test result
     end
 
-    @testset "end to end, pdf is not evaluated at infinity" begin
+    @testset "rand(z) is type stable even when pdf is not" begin
+        f = x -> x < 0.5 ? 1 : 0.5
         @testset for T in TestTypes, N in [1, 2, 3, 4]
-            try
-                z = ziggurat(f, (T(0), T(Inf)), N)
-                rand(z, 1000)
-            catch
-                result = false
-            else
-                result = true
-            end
+            z = ziggurat(f, (T(0), T(1)), N)
+            # Type stable, not type grounded, so we use @inferred instead of @report_opt
+            @test @inferred(rand(z)) isa T
+        end
+    end
 
-            @test result
+    @testset "integers get promoted to floats wherever necessary" begin
+        f = x -> x < 1 ? 2 : 1 # evaluates to Int
+        invf = y -> 1 <= y <= 2 ? 1 : 2 # evaluates to Int
+        @testset for N in [1, 2, 3, 4], ipdf in [nothing, invf]
+            z = ziggurat(f, (0, 2), N; ipdf)
+            @test eltype(z) <: AbstractFloat
+            @test Ziggurats.Ytype(z) <: AbstractFloat
+            @test @inferred(rand(z)) isa AbstractFloat
         end
     end
 end
+
+nothing
